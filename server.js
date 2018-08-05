1aa4f1315e9fae390d0fff741fae99941fc8a6ec
Ricardo <uoio.rem@gmail.com>
2018-08-05 20:09:48 +0200

    port fixed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers.js');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 console.clear();
 
